e8a8dc11cdbc5cdcf7743f58c41528e3a81b9ee8
xavierfav <xavier.favory@gmail.com>
2018-07-19 23:07:15 +0200

    Change default blank

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,18 +3,18 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=50, blank=True, default=None)
-    description = models.TextField(max_length=2500, blank=True, default=None)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=2500, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class Reference(models.Model):
-    name = models.CharField(max_length=50, blank=True, default=None)
-    description = models.TextField(max_length=2500, blank=True, default=None)
-    url = models.CharField(max_length=250, blank=True, default=None)
-    category = models.ManyToManyField(Category, blank=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=2500, null=True, blank=True)
+    url = models.CharField(max_length=250)
+    category = models.ManyToManyField(Category, symmetrical=False, related_name='references')
 
     def __str__(self):
         return self.name
