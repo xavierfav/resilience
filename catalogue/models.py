7487884872f77726a9d19361d06264371f725d63
xavierfav <xavier.favory@gmail.com>
2018-07-19 22:27:26 +0200

    Create Category & Reference models

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, blank=True, default=None)
+    description = models.TextField(max_length=2500, blank=True, default=None)
+
+    def __str__(self):
+        return self.name
+
+
+class Reference(models.Model):
+    name = models.CharField(max_length=50, blank=True, default=None)
+    description = models.TextField(max_length=2500, blank=True, default=None)
+    url = models.CharField(max_length=250, blank=True, default=None)
+    category = models.ManyToManyField(Category, blank=True)
+
+    def __str__(self):
+        return self.name
