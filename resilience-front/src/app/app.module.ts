0529fb3fc68879479d6042c40312304aa949a2e3
valou5940 <valentin.nerin@gmail.com>
2018-07-29 13:30:18 +0200

    Import services

--- resilience-front/src/app/app.module.ts
+++ resilience-front/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AgGridModule } from 'ag-grid-angular';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { AddItemComponent } from './add-item/add-item.component';
 import { DataTableComponent } from './add-item/data-table/data-table.component';
-import { SearchService } from './add-item/search.service';
+import { AddItemService } from './add-item/add-item.service';
 import { AddCategoryComponent } from './add-item/add-category/add-category.component';
+import { CategoryComponent } from './add-item/data-table/category/category.component';
+import { DataTableService } from './add-item/data-table/data-table.service';
+
 
 const appRoutes: Routes = [
   { path: 'add', component: AddItemComponent },
@@ -26,15 +31,21 @@ const appRoutes: Routes = [
     HeaderComponent,
     AddItemComponent,
     DataTableComponent,
-    AddCategoryComponent
+    AddCategoryComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AgGridModule.withComponents([]),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
-  providers: [ SearchService ],
+  providers: [ 
+    AddItemService,
+    CookieService,
+    DataTableService 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
