f55e67c63fb87fea182a7c6769d6b4c952caad74
VALENTIN NERIN <20009356>
2018-08-10 11:13:18 +0200

    Add data models

---
+++ resilience-front2/src/app/shared/ReferencesImpl.model.ts
@@ -0,0 +1,18 @@
+import { References } from './references.model';
+
+export class ReferencesImpl implements References {
+    id: number;
+    name: String;
+    url: String;
+    description: String;
+    category: String[]
+    
+
+    constructor(references: References) {
+        this.id = references.id;
+        this.name = references.name;
+        this.url = references.url;
+        this.description = references.description;
+        this.category = references.category;
+    }
+}
