90979637959f15e56d522335c1e5b2e40ae134b5
VALENTIN NERIN <20009356>
2018-08-14 13:00:13 +0200

    Clean accordion-form (delete useless variables) & add comments

--- resilience-front2/src/app/add-item/accordion-form/accordion-form.component.ts
+++ resilience-front2/src/app/add-item/accordion-form/accordion-form.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AddItemService } from './add-item.service';
-import { map } from 'rxjs/operators';
-import { ReferencesImpl } from '../../shared/ReferencesImpl.model';
-import { References } from '../../shared/references.model';
 
 @Component({
   selector: 'app-accordion-form',
@@ -15,23 +12,17 @@ export class AccordionFormComponent implements OnInit {
   @Input() index:number;
   @Input() id: number;
   @Input() createOrUpdate: string;
+  @Input() references: any;
+  @Output() panel = new EventEmitter<any>();
   description: any;
   name: any;
   url: any;
-  @Output() panel = new EventEmitter<any>();
-  @Output() submit = new EventEmitter<boolean>()
-  isSubmitted: boolean = false;
-  reference: any;
-  @Input() references: any;
-  uniqueReference;
-  idRef: number;
-  title: any;
   categoryArray: any[] = [];
+  reference: any;
 
   constructor(private addItemService: AddItemService) { }
 
   ngOnInit() {
-     console.log(this.references);
   }
 
   // On Changes, display new values into form
@@ -45,7 +36,6 @@ export class AccordionFormComponent implements OnInit {
   //get categories from add-category whenever the overlay is closed
   getCategories(event) {
     this.categoryArray = event;
-    console.log(this.categoryArray);
   }
 
   // subscription of the post method to add a new reference
@@ -64,8 +54,8 @@ export class AccordionFormComponent implements OnInit {
         });
   }
 
+  // subscription of the put request from the service when updating a reference
   updateReference(reference) {
-    console.log(reference);
     this.addItemService.updateReference(reference)
       .subscribe(
         (ref: any) => {
@@ -79,11 +69,8 @@ export class AccordionFormComponent implements OnInit {
 
   // Action to perform when submitting the form (e.g. a new reference)
   onSubmit(form: NgForm) {
-    console.log('form value when submitting', form.value, this.id);
     this.reference = { id: this.id, name: form.value.name, url: form.value.url, description: form.value.description, category: this.categoryArray };
-    console.log(this.reference);
     this.panel.emit(this.reference);
-    this.submit.emit(!this.isSubmitted);
 
     if (form.form.dirty && this.createOrUpdate == 'Create Reference') {
       this.addReferences(this.reference);
@@ -93,6 +80,5 @@ export class AccordionFormComponent implements OnInit {
 
     this.createOrUpdate = 'Update Reference';
     form.form.markAsPristine;
-    this.isSubmitted = true;
   }
 }
