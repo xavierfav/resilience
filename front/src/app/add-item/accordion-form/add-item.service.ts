220b61ea9ded4ea54cc3161cff0346ac4c9dc219
VALENTIN NERIN <20009356>
2018-08-16 11:34:19 +0200

    Change project name

---
+++ front/src/app/add-item/accordion-form/add-item.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { References } from '../../shared/references.model';
+
+
+@Injectable()
+export class AddItemService {
+
+  url: any = 'api/references';
+  csrfToken: any;
+  headers: any;
+  options: any;
+
+  // build the header
+  constructor(private httpClient: HttpClient, private cookieService: CookieService) { 
+    this.headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'X-CSRFToken': this.getCookie(),
+       observe: 'response'
+    });
+  }
+
+  // get the csrf cookie
+  getCookie() {
+    let csrf = this.cookieService.get("csrftoken");
+    // the Angular HttpHeaders class throws an exception if any of the values are undefined
+    if (typeof(csrf) === 'undefined') {
+      csrf = '';
+    }
+    console.log(csrf);
+   return csrf;
+  }
+
+  // send a new reference (post)
+  sendReferences(data) {
+    this.options = { headers: this.headers};
+    return this.httpClient.post<References>(this.url, data, this.options);
+  }
+
+  // update an existing reference (put)
+  updateReference(data) {
+    this.options = { headers: this.headers};
+    return this.httpClient.put<References>(this.url + '/' + data.id, data, this.options);
+  }
+
+  // get all the references (get)
+  getReferences() {
+    return this.httpClient.get<References[]>(this.url);
+  }
+
+  // get one reference given the id (get)
+  getOneReference(id) {
+    return this.httpClient.get<References>(this.url + '/' + id);
+  }
+
+  // delete a reference given the id (delete)
+  deleteReference(id) {
+    return this.httpClient.delete(this.url + '/' + id);
+  }
+}
